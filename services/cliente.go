77d4f492123e7cd20bf633ffdbbf2be49d8d3c40
vineboneto <vineboneto@gmail.com>
2024-02-18 12:33:54 -0300

    fix

--- services/cliente.go
+++ services/cliente.go
@@ -1,21 +1,42 @@
 package services
 
 import (
+	"sync"
+
 	"github.com/vineboneto/rinha-2024-q1-go/pg"
+)
+
+var (
+	cliente404 = sync.Map{}
 )
 
 func FindCliente(id int64) bool {
 	db := pg.GetDB()
 
-	ID := int64(0)
-	err := db.Raw("select id from clientes where id = ?", id).Scan(&ID).Error
+	v, _ := cliente404.Load(id)
 
-	if err != nil {
+	if v == "404" {
 		return false
 	}
 
-	if ID == 0 {
-		return false
+	if v == nil {
+		type Out struct {
+			ID int64 `gorm:"column:id"`
+		}
+		out := Out{}
+		err := db.Raw("select id from clientes where id = ?", id).Scan(&out).Error
+
+		if err != nil {
+			cliente404.Store(id, "404")
+			return false
+		}
+
+		if out.ID == 0 {
+			cliente404.Store(id, "404")
+			return false
+		}
+
+		cliente404.Store(id, "200")
 	}
 
 	return true
